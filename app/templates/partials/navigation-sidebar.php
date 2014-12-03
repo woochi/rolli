832faa67cfe4476911003f0241dd722cadf6e77f
Mikko Kivelä <mkivela@rallydev.com>
2014-12-03 22:32:24 +0200

    Show logo in sidebar

--- app/templates/partials/navigation-sidebar.php
+++ app/templates/partials/navigation-sidebar.php
@@ -1,6 +1,14 @@
+<?php
+  $default_sidebar_logo = get_template_directory_uri() . "/images/sidebar_logo.svg";
+  $sidebar_logo = get_theme_mod('rolli_footer_logo', $default_footer_logo);
+?>
 <nav class="nav-sidebar" role="navigation">
   <?php rolli_nav(array('theme_location' => 'sidebar-menu')); ?>
-  <p class="copyright" role="contentinfo">
+  <div class="copyright" role="contentinfo">
+    <hr class="sidebar-separator">
+    <div class="sidebar-logo-wrapper">
+      <img class="sidebar-logo" src="<?php echo $sidebar_logo; ?>">
+    </div>
     &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.
-  </p>
+  </div>
 </nav>
